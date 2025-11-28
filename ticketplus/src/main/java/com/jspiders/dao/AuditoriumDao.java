285a19c08a70d285b1bf171f2705d1ee40310c6c
Madhu HS <madhu.s.ty@cars24.com>
2025-11-28 10:52:31 +0530

    Adding Dao Interfaces

---
+++ ticketplus/src/main/java/com/jspiders/dao/AuditoriumDao.java
@@ -0,0 +1,8 @@
+package com.jspiders.dao;
+
+public interface AuditoriumDao {
+    void addAuditorium();
+    void getAuditorium(Long audiId);
+    void updateAuditorium(Long audiId);
+    void deleteAuditorium(Long audiId);
+}
---
+++ ticketplus/src/main/java/com/jspiders/dao/AuditoriumDaoImpl.java
@@ -0,0 +1,23 @@
+package com.jspiders.dao;
+
+public class AuditoriumDaoImpl implements AuditoriumDao{
+    @Override
+    public void addAuditorium() {
+
+    }
+
+    @Override
+    public void getAuditorium(Long audiId) {
+
+    }
+
+    @Override
+    public void updateAuditorium(Long audiId) {
+
+    }
+
+    @Override
+    public void deleteAuditorium(Long audiId) {
+
+    }
+}
